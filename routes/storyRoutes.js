314d23517481cf6267ab141281edb66e72772525
bobbwire <bwirebob4@gmail.com>
2025-09-30 21:47:43 +0300

    Update storyRoutes.js

--- routes/storyRoutes.js
+++ routes/storyRoutes.js
@@ -1,11 +1,32 @@
 // routes/storyRoutes.js
 import express from "express";
-import { getStories, createStory, getStoryById } from "../controllers/storyController.js";
+import {
+  getStories,
+  createStory,
+  getStoryById,
+} from "../controllers/storyController.js";
 
 const router = express.Router();
 
+/**
+ * @route   GET /api/stories
+ * @desc    Fetch all stories
+ * @access  Public
+ */
 router.get("/", getStories);
+
+/**
+ * @route   POST /api/stories
+ * @desc    Create a new story
+ * @access  Public (could be private later with auth)
+ */
 router.post("/", createStory);
+
+/**
+ * @route   GET /api/stories/:id
+ * @desc    Fetch a story by ID
+ * @access  Public
+ */
 router.get("/:id", getStoryById);
 
 export default router;
